a9ee26d54f110d615af884308db3230c7ff5a6ff
seyidaga1 <seyidaga221@gmail.com>
2025-07-22 19:13:58 +0400

    add

---
+++ markdown2html.py
@@ -0,0 +1,9 @@
+import sys
+
+# def convert_heading(line) :
+
+
+
+if __name__ == '__main__' :
+    if len(sys.argv) != 3 :
+        sys.stderr.write("Usage : ./markdown2html.py README.md README.html")
