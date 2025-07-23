8df8b59cd7aa76d2b1afeb326acabfac329f7994
seyidaga1 <seyidaga221@gmail.com>
2025-07-23 18:19:16 +0400

    fix

--- markdown2html.py
+++ markdown2html.py
@@ -37,7 +37,50 @@ def markdown_file(name,output) :
 
     except FileNotFoundError:
         sys.stdout.write(f"Missing {name}\n")
+        sys.exit(1)
 
+
+if __name__ == '__main__' :#!/usr/bin/python3
+
+"""
+markdown2html.py
+
+A simple Markdown to HTML converter.
+
+Usage:
+    ./markdown2html.py README.md README.html
+"""
+
+import sys
+
+def convert_heading(line) :
+    if line.startswith("#") :
+        heading_level = line.count("#")
+        heading_text = line.strip("# ").strip()
+        heading = f"<h{heading_level}>{heading_text}</h{heading_level}>"
+        print(heading)
+        return heading
+
+
+def markdown_file(name,output) :
+    try :
+        with open(name,'r') as file :
+            markdown_lines = file.readlines()
+
+        converted_lines = []
+        for line in markdown_lines :
+            converted_line = convert_heading(line)
+            # converted_line = convert_unordered_list(converted_line)
+            converted_lines.append(f"{converted_line}\n")
+
+        with open(output,'w') as file :
+            for line in converted_lines :
+                file.write(line) 
+
+    except FileNotFoundError:
+        print(f"Missing {name}", file=sys.stderr)
+        sys.exit(1)
+        
 
 
 if __name__ == '__main__' :
