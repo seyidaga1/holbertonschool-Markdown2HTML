f91a201c5ce515f8d0eb1d4e80f994bc38f3e4fb
seyidaga1 <seyidaga221@gmail.com>
2025-07-23 18:28:54 +0400

    fix

--- markdown2html.py
+++ markdown2html.py
@@ -11,8 +11,9 @@ Usage:
 
 import sys
 
-def convert_heading(line) :
-    if line.startswith("#") :
+
+def convert_heading(line):
+    if line.startswith("#"):
         heading_level = line.count("#")
         heading_text = line.strip("# ").strip()
         heading = f"<h{heading_level}>{heading_text}</h{heading_level}>"
@@ -20,30 +21,29 @@ def convert_heading(line) :
         return heading
 
 
-def markdown_file(name,output) :
-    try :
-        with open(name,'r') as file :
+def markdown_file(name, output):
+    try:
+        with open(name, 'r') as file:
             markdown_lines = file.readlines()
 
         converted_lines = []
-        for line in markdown_lines :
+        for line in markdown_lines:
             converted_line = convert_heading(line)
             # converted_line = convert_unordered_list(converted_line)
             converted_lines.append(f"{converted_line}\n")
 
-        with open(output,'w') as file :
-            for line in converted_lines :
-                file.write(line) 
+        with open(output, 'w') as file:
+            for line in converted_lines:
+                file.write(line)
 
     except FileNotFoundError:
         sys.stderr.write(f"Missing {name}\n")
         sys.exit(1)
 
 
-if __name__ == '__main__' :
-    if len(sys.argv) != 3 :
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         sys.exit(1)
-    
-    
-    markdown_file(sys.argv[1],sys.argv[2])
+
+    markdown_file(sys.argv[1], sys.argv[2])
